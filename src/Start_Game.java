bb902588b398dd0ac1b53f716b9d8d4261df2f65
nerusuro <43239858+nerusuro@users.noreply.github.com>
2019-03-31 21:40:20 -0400

    Update Start_Game.java

--- src/Start_Game.java
+++ src/Start_Game.java
@@ -46,60 +46,113 @@ public class Start_Game {
 		pointPlayer = tempPlayerCard1.pointValue() + tempPlayerCard2.pointValue();
 		checkForWin(deck);
 	}
-	public static void checkForWin(Deck deck) {
+	public static int checkForWin(Deck deck) {
+		int turn = 0;
 		if (pointPlayer == 21) {
 			System.out.println("Blackjack! You win!");
-		}
-		else if (tempPlayerCard1.rank() == "A") {
-			countAsP++;
-			if (pointPlayer + 10 == 21) {
-				System.out.println("Blackjack! You win!");
-			}
-			else if (countAsP > 1) {
-				if (pointPlayer + 20 == 21) {
-					System.out.println("Blackjack! You win!");
-				}
-			}
-		}
-		else if (tempPlayerCard2.rank() == "A") {
-			countAsP++;
-			if (pointPlayer + 10 == 21) {
-				System.out.println("Blackjack! You win!");
-			}
-			else if (countAsP > 1) {
-				if (pointPlayer + 20 == 21) {
-					System.out.println("Blackjack! You win!");
-				}
-			}
+			return 0;
 		}
 		else {
+			if (tempPlayerCard1.rank() == "A") {
+				countAsP++;
+				if (pointPlayer + 10 == 21) {
+					System.out.println("Blackjack! You win!");
+					return 0;
+				}
+				else if (countAsP > 1) {
+					if (pointPlayer + 20 == 21) {
+						System.out.println("Blackjack! You win!");
+						return 0;
+					}
+				}
+			}
+			else if (tempPlayerCard2.rank() == "A") {
+				countAsP++;
+				if (pointPlayer + 10 == 21) {
+					System.out.println("Blackjack! You win!");
+					return 0;
+				}
+				else if (countAsP > 1) {
+					if (pointPlayer + 20 == 21) {
+						System.out.println("Blackjack! You win!");
+						return 0;
+					}
+				}
+			}
 			if (pointPlayer > 21) {
 				System.out.println("Bust! You lose!");
+				return 0;
+			}
+			else if (turn > 0) {
+				System.out.println("Dealer's Hidden Card was " + tempDealerCard2);
+				while (pointDealer < 21) {
+					tempDealerCard1 = deck.deal();
+					System.out.println("Dealer got : " + tempDealerCard1);
+					pointDealer = pointDealer + tempDealerCard1.pointValue();
+				}
+				if (pointDealer == 21) {
+					System.out.println("You lose!");
+					return 0;
+				}
+				else if (pointDealer > 21) {
+					System.out.println("You lose!");
+					return 0;
+				}
+				else if (Math.abs(pointPlayer - 21) > Math.abs(pointDealer - 21)) {
+					System.out.println("You lose!");
+					return 0;
+				}
+				else {
+					System.out.println("You win!");
+					return 0;
+				}
 			}
 			else {
 				System.out.println("Hit or Stand?");
 				System.out.println("Enter 'H' or 'S' or 'Q' (to quit)");
 				String choice = in.nextLine();
-				while (!choice.equals("Q")) {
-					switch (choice) {
-					case "H":
-						tempPlayerCard1 = null;
-						tempPlayerCard2 = null;
-						tempDealerCard1 = null;
-						tempDealerCard2 = null;
-						break;
-					case "S":
-						tempPlayerCard1 = null;
-						tempPlayerCard2 = null;
-						tempDealerCard1 = null;
-						tempDealerCard2 = null;
-						break;
-					default:
-						System.out.println("Please enter valid command.");
-						break;
-					}
+				switch (choice) {
+				case "H":
+					tempPlayerCard1 = deck.deal();
+					System.out.println("You got: " + tempPlayerCard1);
+					pointPlayer = pointPlayer + tempPlayerCard1.pointValue();
+					turn++;
+					checkForWin(deck);
+					break;
+				case "S":
+					turn++;
+					checkForWinStand(deck);
+					break;
+				default:
+					System.out.println("Please enter valid command.");
+					break;
 				}
 			}
+			return 0;
+		}
+	}
+	public static int checkForWinStand(Deck deck) {
+		System.out.println("Dealer's Hidden Card was " + tempDealerCard2);
+		while (pointDealer < 21) {
+			tempDealerCard1 = deck.deal();
+			System.out.println("Dealer got : " + tempDealerCard1);
+			pointDealer = pointDealer + tempDealerCard1.pointValue();
+		}
+		if (pointDealer == 21) {
+			System.out.println("You lose!");
+			return 0;
+		}
+		else if (pointDealer > 21) {
+			System.out.println("You win!");
+			return 0;
+		}
+		else if (Math.abs(pointPlayer - 21) > Math.abs(pointDealer - 21)) {
+			System.out.println("You lose!");
+			return 0;
+		}
+		else {
+			System.out.println("You win!");
+			return 0;
 		}
 	}
 }
